18dae6ac995bdffe793501d3e4f0deaad9a301cb
cobeliii <cobeliiimuname@gmail.com>
2025-07-12 19:13:42 -0500

    more changes made to these classes

--- src/test/java/com/cobeliii/AppTest.java
+++ src/test/java/com/cobeliii/AppTest.java
@@ -2,11 +2,14 @@ package com.cobeliii;
 
 
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 public class AppTest {
@@ -63,5 +66,20 @@ public class AppTest {
         //Assert
         verify(listMock, times(2)).add("Hello");
         verifyNoMoreInteractions(listMock);
+    }
+
+    @Test
+    void mockitoBdd() {
+        //given
+        List<String> mockList = mock();
+        //when(mockList.get(0)).thenReturn("hello");
+        given(mockList.get(0)).willReturn("hello");
+        //when
+        String actual = mockList.get(0);
+
+        //Assert
+        verify(mockList).get(0);
+        then(mockList).should().get(0);
+        assertThat(actual).isEqualTo("hello");
     }
 }
--- src/test/java/com/cobeliii/OrderServiceTest.java
+++ src/test/java/com/cobeliii/OrderServiceTest.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,21 +23,55 @@ class OrderServiceTest {
     @Mock
     private OrderRepository orderRepository;
 
+    @Captor
+    private ArgumentCaptor<Order> orderArgumentCaptor;
+
     @InjectMocks
     private OrderService underTest;
 
 
     @Test
-    void itShouldCharge() {
+    void itShouldChargeWithAssertArg() {
         //given
         BigDecimal amount = BigDecimal.TEN;
+        User user = new User(1, "James");
         when(paymentProcessor.charge(amount)).thenReturn(true);
         when(orderRepository.save(any())).thenReturn(1);
         //when
-        boolean actual = underTest.processOrder(null ,amount);
+        boolean actual = underTest.processOrder(user ,amount);
 
         //Assert
         verify(paymentProcessor).charge(amount);
+        verify(orderRepository).save(assertArg(order -> {
+            assertThat(order.amount()).isEqualTo(amount);
+            assertThat(order.user()).isEqualTo(user);
+            assertThat(order.id()).isNotNull();
+            assertThat(order.orderDate()).isBefore(ZonedDateTime.now()).isNotNull();
+        }));
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void itShouldChargeWithArgCaptor() {
+        //given
+        BigDecimal amount = BigDecimal.TEN;
+        User user = new User(1, "James");
+        when(paymentProcessor.charge(amount)).thenReturn(true);
+        when(orderRepository.save(any())).thenReturn(1);
+        //when
+        boolean actual = underTest.processOrder(user ,amount);
+
+//        ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
+
+        //Assert
+        verify(paymentProcessor).charge(amount);
+        verify(orderRepository).save(orderArgumentCaptor.capture());
+        Order orderArgumentCaptorValue = orderArgumentCaptor.getValue();
+
+        assertThat(orderArgumentCaptorValue.user()).isEqualTo(user);
+        assertThat(orderArgumentCaptorValue.amount()).isEqualTo(amount);
+        assertThat(orderArgumentCaptorValue.orderDate()).isBefore(ZonedDateTime.now()).isNotNull();
+        assertThat(orderArgumentCaptorValue.id()).isNotNull();
         assertThat(actual).isTrue();
     }
 
